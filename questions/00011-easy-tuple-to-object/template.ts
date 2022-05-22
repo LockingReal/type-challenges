c859298c8d41dc491a27dddb5ed0dac30a0e06cb
LockingReal <1191515088@qq.com>
2022-05-22 11:19:08 +0800

    11 over

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,13 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly any[]> = {
+    [P in T[number]]:P
+}
+
+
+// in js
+// function tupleToObject(array){
+//     const obj = {};
+//     array.forEach((val)=>{
+//         obj[val] = val;
+//     })
+//     return obj;
+// }
