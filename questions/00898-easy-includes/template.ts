0c7ae46ef8b3a35f3bd27c324e6dc05fe468dfa5
LockingReal <1191515088@qq.com>
2022-05-22 19:37:29 +0800

    889 over

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any
+import { Equal } from "@type-challenges/utils"
+
+export type Includes<T extends any[], U> = T extends [infer First,...infer Rest]?Equal<First,U> extends true?true:Includes<Rest,U>:false;
+
+// 递归
+// T extends [infer First,...infer Rest]? // 去除当前的这一项
+// Equal<First,U> extends true?true:Includes<Rest,U>:false;// 这次 First 这一项 相等为true了吗？是就返回，不是就 继续递归,递归结束,返回false
+
