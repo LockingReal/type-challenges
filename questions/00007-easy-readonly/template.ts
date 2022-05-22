304560e455ef7cdb7e3189dfcc33a74ab00135db
LockingReal <1191515088@qq.com>
2022-05-22 10:59:35 +0800

    7 over

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,13 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+    readonly [key in keyof T]:T[key]
+};
+
+// in js
+// function readonly(obj){
+//     const result = {};
+//     在 相当于 keyof
+//     for(const key in obj){
+//         result["readonly"+key] = obj[key];
+//     }
+//     return result;
+// }
