3974ba6fb14b2606b1c0698295586c23766ed39a
LockingReal <1191515088@qq.com>
2022-05-22 18:21:23 +0800

    189 over

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,4 +1,4 @@
 type MyPick<T, K extends keyof T> = {
-    [key in K]:T[key]
+    [key in  K]:T[key]
 };
 // 2022/5/22
--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,2 @@
-type MyAwaited = any
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer Z>? Z extends Promise<unknown>?MyAwaited<Z>:Z:T;
+//2022/5/22
